31fd806c9ba9610e75bf1a57a24fadaacdb5d4a1
HansKristianNielsen <53754289+HansKristianNielsen@users.noreply.github.com>
2019-10-25 10:46:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <title>golden shoe</title>
+     <meta name="description" content="DESCRIPTION">
+    <link rel="stylesheet" href="PATH">
+
+     <!--[if lt IE 9]>
+       <script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+     <![endif]-->
+ </head>
+
+ <body>
+<h1>The golden shoe</h1>
+
+
+<form  action="" method="post">
+  <div>
+  <label for="select-brand">Select golden option:</label>
+ <select name="selectGolden" id="select_golden">
+   <option value="golden">golden</option>
+   <option value="not_golden">not golden</option>
+ </select>
+</div>
+
+<div>
+ <label for="select-brand">Select brand:</label>
+<select name="selectBrand" id="select_brand">
+  <option value="">All</option>
+  <option value="addidas">Addidas</option>
+  <option value="nike">Nike</option>
+  <option value="reebok">Reebok</option>
+</select>
+<div>
+
+  <input type="submit" name="submit" value="Søg" />
+</form>
+
+
+<?php
+// include the model code to acces the functions
+include "model/shoemodel.php";
+
+
+// check for post
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+
+
+// get post result
+$myShoes=brandBased($_POST["selectBrand"]);
+//$myShoes=goldBased($_POST["selectGolden"]);
+
+//var_dump($myShoes);
+
+//view
+echo "<section>";
+
+foreach ($myShoes as $shoe) {
+  echo "<article>";
+
+    $shoeImg=$shoe["image"];
+    $shoeBrand=$shoe["brand"];
+    $stature=$shoe["golden"];
+    $shoeSize=$shoe["size"];
+
+    echo '<img src="img/'.$shoeImg.'">';
+    echo "<h2> Mærke: ".$shoeBrand."</h2>";
+    if ($stature == true) {
+      echo "<p> Golden </p>";
+    } else{
+      echo " ";
+    }
+    echo "<p> Størrelse: ".$shoeSize."</p>";
+  echo "</article>";
+}
+echo "</section>";
+
+}
+
+
+?>
+
+
+
+ </body>
+
+ </html>
